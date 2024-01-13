ff0483957cd65685f4dbfb69dbb02e24c13fd3f9
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-01-13 18:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	// Create a Blob with the string content and specify the MIME type as plain text
+    const blob = new Blob([str], { type: 'text/plain' });
+
+    // Get the size of the Blob in bytes
+    const sizeInBytes = blob.size;
+
+    return sizeInBytes;
 };
 
 // Do not change the code below
